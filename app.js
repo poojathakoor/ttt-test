26a35b42b3a2079ba0c3541f348eaec4fc9fff7f
pooja-thakoor <pooja.thakoor18@gmail.com>
2017-12-04 12:47:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    engines = require('consolidate'),
+    path = require('path'),
+    request = require('request'),
+    minHeap = require('./min-heap');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+//index
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.get('/words/:count', function(req, res) {
+    console.log(req.params['count']);
+    var n = parseInt(req.params['count']);
+    request.get('http://terriblytinytales.com/test.txt', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var heap = new minHeap(function(l, r) {
+                return l.weight - r.weight;
+            });
+            var words = body.match(/\b(\w+)\b/g);
+            var wordCountsMap = words.reduce(function(memo, word) {
+                memo[word] = (memo[word] || 0) + 1;
+                return memo;
+            }, {});
+
+            var j = 0
+            for (var word in wordCountsMap) {
+                if (j < n)
+                    heap.insert({ weight: wordCountsMap[word], id: word });
+                else if (heap.getHead() && heap.getHead().weight < wordCountsMap[word])
+                    heap.replaceHead({ weight: wordCountsMap[word], id: word });
+                j++;
+            }
+            
+            res.send(heap.getHeap())
+        }
+    });
+});
+
+
+app.listen(process.env.PORT || 3000);
